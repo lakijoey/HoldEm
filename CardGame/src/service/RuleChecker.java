969b6fa501a0d2ec55128077f8179b19dd99d5e8
Laki <lakijoey@gmail.com>
2021-01-21 19:16:29 +0100

    .

--- CardGame/src/service/RuleChecker.java
+++ CardGame/src/service/RuleChecker.java
@@ -48,7 +48,7 @@ public class RuleChecker {
         return (c >= 5 || h >= 5 || d >= 5 || s >= 5);
     }
 
-    public static boolean checkStraight(ArrayList<Integer> sevenCards, Profile profile) {
+    public static boolean checkStraight(ArrayList<Integer> sevenCards) {
         int db = 1;
         //check if cards follow each other
         for (int j = 0; j < sevenCards.size()-1; j++) {
@@ -63,7 +63,7 @@ public class RuleChecker {
     }
 
 
-    public static int getSameCardsNumber(ArrayList<Integer> sevenCards, Profile profile) {
+    public static int getOccurances(ArrayList<Integer> sevenCards) {
         HashMap<Integer, Integer> incidences = new HashMap<>();
         for (int i = 0; i < sevenCards.size(); i++) {
             if (incidences.containsKey(sevenCards.get(i))) {
@@ -72,20 +72,49 @@ public class RuleChecker {
                 incidences.put(sevenCards.get(i), 1);
             }
         }
-        return Collections.max(incidences.values());
+        int inc = Collections.max(incidences.values());
+        if ((Collections.frequency(incidences.values(), 2) == 2 || Collections.frequency(incidences.values(), 2) == 3) && (inc == 2)){
+            inc = 2;
+        }
+        if ((Collections.frequency(incidences.values(), 2) == 1 && inc == 2)){
+            inc = 10;
+        }
+        if ((Collections.frequency(incidences.values(), 2) == 1 || Collections.frequency(incidences.values(), 2) == 2) && (inc == 3)){
+            inc = 5;
+        }
+        return inc;
 
     }
 
     public static boolean checkPoker(Profile profile) {
-        return getSameCardsNumber(getSevenCards(profile),profile) == 4;
+        return getOccurances(getSevenCards(profile)) == 4;
     }
-
-
-
+    
     public static boolean checkThreeOfAKind(Profile profile) {
-        return getSameCardsNumber(getSevenCards(profile),profile) == 3;
+        return getOccurances(getSevenCards(profile)) == 3;
     }
 
+    public static boolean checkTwoPair(Profile profile){
+        return getOccurances(getSevenCards(profile)) == 2;
+    }
+
+    public static boolean checkOnePair(Profile profile){
+        return getOccurances(getSevenCards(profile)) == 10;
+    }
+    public static boolean checkStraightFlush(Profile profile){
+        return (checkFlush(profile) && checkStraight(getSevenCards(profile)));
+    }
+    
+    public static boolean checkRoyalFlush(Profile profile){
+        return (checkFlush(profile) && checkStraight(getSevenCards(profile)) && getSevenCards(profile).containsAll(Arrays.asList(1, 10, 11, 12, 13)));
+    }
+    
+    public static boolean checkFullHouse(Profile profile){
+        return getOccurances(getSevenCards(profile)) == 5;
+    }
+    public static int getHighCardValue(Profile profile){
+        return Collections.max(getSevenCards(profile));
+    }
 }
 
 
@@ -117,15 +146,14 @@ public class RuleChecker {
 
 //TODO
     /*
-    Remove card if Fold
-    Royal Flush- A, K, Q, J, 10, all in the same suit. 1 in 650,000
-   .... DoubleCheck(Flush, Straight) Straight Flush - Five cards in sequence, all of the same suit. 1in 65,000
+   .. Royal Flush- A, K, Q, J, 10, all in the same suit. 1 in 650,000
+   .. Straight Flush - Five cards in sequence, all of the same suit. 1in 65,000
    .. Four of a Kind- Four cards of one rank. Kicker breaks ties. 1 in 4,000
-   .... DoubleCheck(One Pair, Three of..)Full House- Three matching cards of one rank, plus Two matching cards of another rank. Higher ranking set of three wins. If two players have the same set of three, the player with the higher pair wins. 1 in 700
+   .. Full House- Three matching cards of one rank, plus Two matching cards of another rank. Higher ranking set of three wins. If two players have the same set of three, the player with the higher pair wins. 1 in 700
    .. Flush- Five cards of the same suit. High card wins. 1 in 500
    .. Straight- Five cards of sequential rank, but different suit. High card wins. 1 in 250
    .. Three of a kind- Three cards of the same rank, plus two unmatched cards. High set wins. 1 in 50
-    Two Pair- Two cards of the same rank, plus Two cards of another rank. High pair wins. 1 in 20
-    One Pair- Two cards of the same rank, plus Three unmatched cards. High pair wins. 1 in 2 1/3
+   .. Two Pair- Two cards of the same rank, plus Two cards of another rank. High pair wins. 1 in 20
+   .. One Pair- Two cards of the same rank, plus Three unmatched cards. High pair wins. 1 in 2 1/3
     High Card- One card high, plus four unmatched lower ranking cards. Ace is the Highest card. Kicker breaks ties. 1 in 1
      */
